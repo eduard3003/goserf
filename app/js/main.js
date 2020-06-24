42462042662b4229cfa5fdb357d514650c75d28d
Eduard <andreeveduard1@gmail.com>
2020-06-24 12:16:33 +0300

    Add map and finished header

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
-$(function(){
+$(function () {
 
-$('.header-slider').slick({
-    infinite: true,
-    fade: true
+    $('.header-slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
+        asNavFor: '.slider-dots'
+    });
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header-slider',
+    });
 
-});
 
-  
 });
