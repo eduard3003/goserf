0aec6bc11da4bb914bf132481781aaf805462d44
Eduard <andreeveduard1@gmail.com>
2020-07-19 21:27:18 +0300

    add sliders

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,28 @@ $(function () {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header-slider',
+    });
+
+    $('.serf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf-slider',
+        focusOnSelect: true,
     });
 
 
