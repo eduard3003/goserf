217b8a603773053ef7817db711bf957ed62dae80
Eduard <andreeveduard1@gmail.com>
2020-07-22 21:27:11 +0300

    add travel

--- app/js/main.js
+++ app/js/main.js
@@ -29,5 +29,12 @@ $(function () {
         focusOnSelect: true,
     });
 
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
+    });
+
 
 });
