be089e03cb54f488cc13067eac90369214e9a2cf
Eduard <andreeveduard1@gmail.com>
2020-10-03 11:03:48 +0300

    Add shoop and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,102 @@
 $(function () {
 
-    $('.header-slider').slick({
-        infinite: true,
-        fade: true,
-        prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
-        asNavFor: '.slider-dotshead'
-    });
-    $('.slider-dotshead').slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        asNavFor: '.header-slider',
+  $('.header-slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
+    asNavFor: '.slider-dotshead'
+  });
+  $('.slider-dotshead').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header-slider',
+  });
+
+  $('.serf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
+    asNavFor: '.slider-map',
+  });
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.serf-slider',
+    focusOnSelect: true,
+  });
+
+  $('.holder__slider, .shop__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
+  });
+
+  $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg" alt="plus"></div><div class="quantity-button quantity-down"><img src="images/minus.svg" alt="minus"></div></div>').insertAfter('.quantity input');
+  $('.quantity').each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
     });
 
-    $('.serf-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
-        asNavFor: '.slider-map',
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
     });
 
-    $('.slider-map').slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        arrows: false,
-        asNavFor: '.serf-slider',
-        focusOnSelect: true,
+  });
+
+  $('.quantity-button').on('click', function(){
+    
+  let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+  $('.summ').html('$' + summ);
+  });
+  $('.quantity-button').on('click', function(){
+    var parents = $(this).parents('.holder-slider__info');
+     let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+     $('.summ', parents).html('$' + summ);
+    });
+    
+    
+    $('.quantity').each(function() {
+    var parents = $(this).parents('.holder-slider__info');
+    let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+     $('.summ', parents).html('$' + summ);
     });
 
-    $('.travel__slider').slick({
-        infinite: true,
-        fade: true,
-        prevArrow: '<img class="slider-arrows slider-arrows__left " src="images/arrows-left.svg" alt="arrows"></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right " src="images/arrows-rigth.svg" alt="arrows"></img>',
-    });
 
+
+
+  let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+  $('.summ').html('$' + summ);
+
+  $('.surfboard-box__circle').on('click', function(){
+    $(this).toggleClass('active');
+  });
+ 
+  
 
 });
